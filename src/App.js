41c482197988ba3b47afed09d26b2051c5bbf5be
Shivani Yadav <nteasocial@gmail.com>
2022-03-25 23:41:17 +0530

    seond

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/MenuComponent";
 import "./App.css";
+import { DISHES } from "./shared/dishes";
+import { render } from "react-dom";
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Hello👋🏼</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Hello👋🏼</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
-
 export default App;
